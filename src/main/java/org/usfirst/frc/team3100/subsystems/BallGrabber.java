f3d84fdcbe362e4633a98743a7d5131afcd84bcd
aiden7301 <aidenaaj@gmail.com>
2017-01-11 16:40:03 -0600

    Working ball pickup system

---
+++ src/main/java/org/usfirst/frc/team3100/subsystems/BallGrabber.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team3100.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team3100.RobotMap;
+
+
+public class BallGrabber extends Subsystem {
+
+    private SpeedController grabMotor = RobotMap.ballMotor;
+
+    public void initDefaultCommand() {
+
+    }
+
+    public void start() {
+        grabMotor.set(1);
+    }
+
+    public void stop() {
+        grabMotor.set(0);
+    }
+}
