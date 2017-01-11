47120fd8efaa11286d992761f0d6dfc3512fae0c
aiden7301 <aidenaaj@gmail.com>
2017-01-11 09:26:32 -0600

    Shooter control

---
+++ src/main/java/org/usfirst/frc/team3100/commands/Shoot.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team3100.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3100.Robot;
+
+public class Shoot extends Command {
+    public Shoot() {
+        super("Shoot");
+        requires(Robot.shooter);
+        setTimeout(2);
+    }
+
+    public void initialize() {
+        Robot.shooter.shoot();
+    }
+
+    public void execute() {
+
+    }
+
+    public boolean isFinished() {
+        return isTimedOut();
+    }
+
+    public void end() {
+        Robot.shooter.stopShooting();
+    }
+
+    public void interrupted() {
+        end();
+    }
+}
+
